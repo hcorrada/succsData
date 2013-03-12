9ecdf50841651e7f79e1e5c9f65b77cf8f460ef4
Hector Corrada Bravo <hcorrada@cs.wisc.edu>
2013-03-12 15:28:05 -0400

    apData_gpl570 documented

--- makeData.r
+++ makeData.r
@@ -2,20 +2,36 @@ expDir="~/barcode/exps/succs/rafaSuccs"
 load(file.path(expDir,"rdas/gpl570.rda"))
 
 
-ii=tab$ExperimentID %in% c("GSE8671","GSE4183") & goodIndex & !(is.na(tab$Status))
+require(Biobase)
+keep=goodIndex & !is.na(tab$Status)
+exprs=zscores[,keep]
+pd=tab[keep,]
+
+rownames(pd)=pd$DB_ID
+colnames(exprs)=rownames(pd)
+
+apData_gpl570=ExpressionSet(assayData=exprs,phenoData=AnnotatedDataFrame(pd))
+annotation(apData_gpl570) <- "hgu133plus2"
+
+if (!file.exists("pkg/data"))
+  dir.create("pkg/data")
+save(apData_gpl570, file="pkg/data/apData_gpl570.rda")
+
+
+ii=apDataPlus2$ExperimentID %in% c("GSE8671","GSE4183") & goodIndex & !(is.na(tab$Status))
 colonTab=tab[ii,]
 colonExpr=zscores[,ii]
 
-load(file.path(expDir,"rdas/anno.rda"))
 
 # look at probesets in blocks
+load(file.path(expDir,"rdas/anno.rda"))
+
 blockIds=subset(anno,inBlock)$affyid
 blockExpr=colonExpr[blockIds,]
 
 rownames(colonTab)=colonTab$DB_ID
 colnames(blockExpr)=colonTab$DB_ID
 
-require(Biobase)
 colonData=ExpressionSet(assayData=blockExpr,phenoData=AnnotatedDataFrame(colonTab))
 
 save(colonData, file="pkg/data/colonData.rda")
--- pkg/R/antiProfilesData-package.r
+++ pkg/R/antiProfilesData-package.r
@@ -1,5 +1,7 @@
-#' pkg
+#' antiProfilesData
 #'
-#' @name pkg
+#' Curated dataset of many normal tissue samples and cancer samples. Data are z-scores for the gene expression barcode implemented in the frma package.
+#' 
+#' @name antiProfilesData-package
 #' @docType package
 NULL
