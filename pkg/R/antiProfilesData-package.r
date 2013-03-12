d8148e09976a53eca61ed96498774165f1d487a4
Hector Corrada Bravo <hcorrada@cs.wisc.edu>
2013-03-12 15:28:24 -0400

    removing package rd file

--- pkg/R/antiProfilesData-package.r
+++
@@ -1,7 +0,0 @@
-#' antiProfilesData
-#'
-#' Curated dataset of many normal tissue samples and cancer samples. Data are z-scores for the gene expression barcode implemented in the frma package.
-#' 
-#' @name antiProfilesData-package
-#' @docType package
-NULL
