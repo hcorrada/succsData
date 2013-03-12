00741e4105bdaf32b4ca37692230916fc514dde7
Hector Corrada Bravo <hcorrada@cs.wisc.edu>
2013-03-12 16:31:40 -0400

    cleaning up old rda file

--- pkg/R/apColonData.r
+++ pkg/R/apColonData.r
@@ -1,6 +1,6 @@
 #' Curated dataset of many colon normal and cancer samples on Affymetrix hgu133plus2 expression arrays. 
 #'
-#' Data used in Corrada Bravo, et al. gene expression anti-profiles paper:  BMC Bioinformatics 2012, 13:272 doi:10.1186/1471-2105-13-272. Measurements are z-scores obtained from the GeneExpression Barcode in the 'frma' package
+#' Data used in Corrada Bravo, et al. gene expression anti-profiles paper:  BMC Bioinformatics 2012, 13:272 doi:10.1186/1471-2105-13-272. Measurements are z-scores obtained from the GeneExpression Barcode in the 'frma' package. Only probes mapped to genes within colon cancer hypo-methylation blocks defined in Hansen et al. are included.
 #'
 #' @author Hector Corrada Bravo
 #' @section format: Data is an \code{\link{ExpressionSet}} object. The \code{exprs} slot contains gene expression barcode z-scores from \code{frma} preprocessed data. The \code{phenoData} slot contains a data frame with the following columns: \describe{
@@ -13,10 +13,17 @@
 #'     \item{\code{Status}:}{Normal (0) or Cancer (1) indicator}
 #'   }
 #'
-#' @seealso \code{\link{ExpressionSet}} for the class definition, \code{\link{frma}} for the preprocessing method used, \code{\link{barcode}} for the function to obtain the z-score definition, \code{\link{apStats}} and \code{\link{buildAntiProfile}} for methods to build anti-profiles from this data.
+#' @examples
+#' data(apColonData)
+#' pData(apColonData)
+#'
+#' @seealso \code{\link{ExpressionSet}} for the class definition, \code{\link[frma]{frma}} for the preprocessing method used, \code{\link[frma]{barcode}} for the function to obtain the z-score definition, \code{\link{apStats}} and \code{\link{buildAntiProfile}} for methods to build anti-profiles from this data.
 #'
 #' @references
 #'   Corrada Bravo, H., Pihur, V., McCall, M., Irizarry, R.A., Leek, J.T. (2012). "Gene expression anti-profiles as a basis for accurate universal cancer signatures" BMC Bioinformatics, 13:272
+#'
+#' Hansen, K. D., Timp, W., Bravo, H. C., Sabunciyan, S., Langmead, B., McDonald, O. G., Wen, B., et al. (2011). "Increased methylation variation in epigenetic domains across cancer types." Nature Genetics, 43(8), 768
+#'
 #' @name apColonData
 #' @docType data
 NULL
